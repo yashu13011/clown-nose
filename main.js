1dde23acdc254c9201e9b71a4b9efaf5f3834d69
yashu13011 <83954702+yashu13011@users.noreply.github.com>
2022-06-14 11:26:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+noseX =0;
+noseY =0;
+
+function preload(){
+   clown_nose = loadImage('https://i.postimg.cc/6pLvdhgY/clown-removebg-preview.png');
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose" , gotPoses);
+}
+function draw(){
+    image(video,0,0,300,300);
+    image(clown_nose,noseX,noseY,40,40);
+}
+function takeSnapshot(){
+    save('myfilterimg.png');
+}
+function modelLoaded(){
+    console.log(" poseNet model is loaded!");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15; 
+    }
+}
